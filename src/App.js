7e7760f07c4cc8fe110781220ebe2b162ad9a2cb
seungtoctoc <dltmxor31@naver.com>
2024-01-24 15:11:05 +0900

    feat: fetch from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Writing from './components/Writing'
+
 function App() {
+  const [writings, setWritings] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        return response.json();
+    })
+    .then(posts => {
+      setWritings(posts);
+    })
+    .then(() => {
+      writings.forEach(cpost => {
+        console.log(cpost.id, cpost.title);
+      });
+    })
+  }, [])
+  
+    
+
+  
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* <Writing></Writing> */}
+
+
     </div>
   );
 }
